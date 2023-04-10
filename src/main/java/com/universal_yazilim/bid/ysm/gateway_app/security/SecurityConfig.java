684b566039d62388a2fcc615112b97cd09177ba4
nyerinde <nyerinde@iski.gov.tr>
2023-04-10 09:21:38 +0300

    Initial commit

---
+++ src/main/java/com/universal_yazilim/bid/ysm/gateway_app/security/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.universal_yazilim.bid.ysm.gateway_app.security;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+/*
+Web tabanlı güvenlik sağlamak için
+    @EnableWebSecurity annotation i
+    ve WebSecurityConfigurerAdapter
+    sınıfı kullanıldı
+
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+    @Value("${service.security.secure-key-username}")
+    private String secureKeyUsername;
+
+    @Value("${service.security.secure-key-password}")
+    private  String secureKeyPassword;
+
+    @Bean
+   public PasswordEncoder createPasswordEncode()
+   {
+       return new BCryptPasswordEncoder();
+   }
+
+    /*
+        Uygulamanın kullanıcı ad ve parola bilgilerinin
+        belirlenen bilgiler olması için
+        nu metot "override" edilir.
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception
+    {
+        PasswordEncoder encoder= new BCryptPasswordEncoder();
+
+        // https://docs.spring.io/spring-security/reference/servlet/authentication/passwords/in-memory.html
+        // https://www.baeldung.com/spring-security-5-default-password-encoder
+
+       auth.inMemoryAuthentication()
+               .passwordEncoder(encoder)
+               .withUser(secureKeyUsername)
+               .password(encoder.encode(secureKeyPassword))
+               .roles("USER");
+
+    }
+
+    //Session kullanılmayacak. JSON Web Token kullanılacak
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception
+    {
+        super.configure(httpSecurity);
+
+        //CSRF -> Cross Site Request Forgery
+        httpSecurity.csrf().disable();
+    }
+
+
+}
